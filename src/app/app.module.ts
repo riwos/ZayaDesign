b91712b2256003f1e3cbc49acba9bbf6f2fc136c
Jakub <jakub.nazim@gmail.com>
2018-03-18 23:48:18 +0000

    Added slider component and jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { ZayaHeaderComponent } from './zaya-header/zaya-header.component';
+import { ZayaSliderComponent } from './zaya-slider/zaya-slider.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    ZayaHeaderComponent
+    ZayaHeaderComponent,
+    ZayaSliderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/zaya-slider/zaya-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zaya-slider',
+  templateUrl: './zaya-slider.component.html',
+  styleUrls: ['./zaya-slider.component.css']
+})
+export class ZayaSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
