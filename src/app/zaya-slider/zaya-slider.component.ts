3c8960dc2092abb667a9b422eef21c398181beaa
Jakub <jakub.nazim@gmail.com>
2018-03-19 00:28:31 +0000

    Added zaya-slider component

--- src/app/zaya-slider/zaya-slider.component.ts
+++ src/app/zaya-slider/zaya-slider.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+declare let $: any;
+declare let flexslider: any;
 
 @Component({
   selector: 'app-zaya-slider',
   templateUrl: './zaya-slider.component.html',
   styleUrls: ['./zaya-slider.component.css']
 })
-export class ZayaSliderComponent implements OnInit {
+export class ZayaSliderComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('zayaslider') ctrlSlider: ElementRef;
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    $(this.ctrlSlider.nativeElement).flexslider();
   }
 
 }
