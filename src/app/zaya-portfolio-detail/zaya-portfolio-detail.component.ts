0625ae74c329cea51c506ff7bcafd3eb55bef6ed
Jakub <jakub.nazim@gmail.com>
2018-03-19 14:17:28 +0000

    Added portfolio detail component

---
+++ src/app/zaya-portfolio-detail/zaya-portfolio-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zaya-portfolio-detail',
+  templateUrl: './zaya-portfolio-detail.component.html',
+  styleUrls: ['./zaya-portfolio-detail.component.css']
+})
+export class ZayaPortfolioDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
