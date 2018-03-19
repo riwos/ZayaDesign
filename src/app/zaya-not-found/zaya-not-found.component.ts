affdf03fc31807f36d44bc8f8f8120a81f60777d
Jakub <jakub.nazim@gmail.com>
2018-03-19 14:16:10 +0000

    Added not found component

---
+++ src/app/zaya-not-found/zaya-not-found.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zaya-not-found',
+  templateUrl: './zaya-not-found.component.html',
+  styleUrls: ['./zaya-not-found.component.css']
+})
+export class ZayaNotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
