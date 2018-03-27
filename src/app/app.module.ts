cd0c9995d023b48951da58d9ea7868d7695c9714
riwos <jakub.nazim@gmail.com>
2018-03-27 21:21:25 +0100

    Added lightbox to first approach of gallery and removed portfoliodetail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,12 @@ import { ZayaPortfolioComponent } from './zaya-portfolio/zaya-portfolio.componen
 import { ZayaNotFoundComponent } from './zaya-not-found/zaya-not-found.component';
 import { ZayaMainPageComponent } from './zaya-main-page/zaya-main-page.component';
 import { ZayaContactComponent } from './zaya-contact/zaya-contact.component';
-import { ZayaPortfolioDetailComponent } from './zaya-portfolio-detail/zaya-portfolio-detail.component';
 import { ZayaScopeComponent } from './zaya-scope/zaya-scope.component';
 import { ScrollToModule } from 'ng2-scroll-to';
 import { ZayaAboutComponent } from './zaya-about/zaya-about.component';
 import { ZayaPortfolioDetailService } from './services/zaya-portfolio-detail.service';
 import { ZayaPortfolioDetail2Component } from './zaya-portfolio-detail-2/zaya-portfolio-detail-2.component';
+import { LightboxModule } from 'angular2-lightbox';
 
 @NgModule({
   declarations: [
@@ -27,7 +27,6 @@ import { ZayaPortfolioDetail2Component } from './zaya-portfolio-detail-2/zaya-po
     ZayaPortfolioComponent,
     ZayaNotFoundComponent,
     ZayaContactComponent,
-    ZayaPortfolioDetailComponent,
     ZayaScopeComponent,
     ZayaAboutComponent,
     ZayaPortfolioDetail2Component
@@ -37,13 +36,13 @@ import { ZayaPortfolioDetail2Component } from './zaya-portfolio-detail-2/zaya-po
     RouterModule.forRoot([
       { path: '', redirectTo: '/home', pathMatch: 'full' },
       { path: 'home', component: ZayaMainPageComponent },
-      { path: 'portfolio/:id', component: ZayaPortfolioDetailComponent},
-      { path: 'portfolio1/:id', component: ZayaPortfolioDetail2Component},
+      { path: 'portfolio/:id', component: ZayaPortfolioDetail2Component},
       { path: 'portfolio', component: ZayaPortfolioComponent},
       { path: 'contact', component: ZayaContactComponent},
       { path: '**', component: ZayaNotFoundComponent }
       ]),
-      ScrollToModule.forRoot()
+      ScrollToModule.forRoot(),
+      LightboxModule 
   ],
   providers: [ZayaPortfolioDetailService],
   bootstrap: [AppComponent]
--- src/app/zaya-portfolio-detail/zaya-portfolio-detail.component.ts
+++ src/app/zaya-portfolio-detail/zaya-portfolio-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Lightbox } from 'angular2-lightbox';
 @Component({
   selector: 'zaya-portfolio-detail',
   templateUrl: './zaya-portfolio-detail.component.html',
@@ -6,8 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ZayaPortfolioDetailComponent implements OnInit {
 
-  constructor() { }
+  private _album = [];
+  
+  constructor(private _lightbox: Lightbox) {
+    // for (let i = 1; i <= 4; i++) {
+    //   const src = 'demo/img/image' + i + '.jpg';
+    //   const caption = 'Image ' + i + ' caption here';
+    //   const thumb = 'demo/img/image' + i + '-thumb.jpg';
+    //   const album = {
+    //      src: src,
+    //      caption: caption,
+    //      thumb: thumb
+    //   };
 
-  ngOnInit() {;
+      let album = {
+        src: 'assets/test/a.jpg',
+        caption: 'Image a',
+        thumb: ''
+      }
+
+      let album1 = {
+        src: 'assets/test/b.jpg',
+        caption: 'Image b',
+        thumb: ''
+      }
+
+      let album2 = {
+        src: 'assets/test/c.jpg',
+        caption: 'Image c',
+        thumb: ''
+      }
+
+      let album3 = {
+        src: 'assets/test/d.jpg',
+        caption: 'Image d',
+        thumb: ''
+      }
+
+      this._album.push(album);
+      this._album.push(album1);
+      this._album.push(album2);
+      this._album.push(album3);
+
   }
+
+  ngOnInit() {
+
+  }
+
+  openLightbox(index: number): void {
+    // open lightbox
+    this._lightbox.open(this._album, index);
+  }
+
+  
 }
