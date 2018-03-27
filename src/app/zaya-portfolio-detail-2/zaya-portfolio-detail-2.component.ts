debad47ac5cca78e3402b95149f672b27efdd90c
riwos <jakub.nazim@gmail.com>
2018-03-27 08:33:56 +0100

    Added temporary two tempalte for portfolio detail

---
+++ src/app/zaya-portfolio-detail-2/zaya-portfolio-detail-2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zaya-portfolio-detail-2',
+  templateUrl: './zaya-portfolio-detail-2.component.html',
+  styleUrls: ['./zaya-portfolio-detail-2.component.css']
+})
+export class ZayaPortfolioDetail2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/zaya-portfolio-detail/zaya-portfolio-detail.component.ts
+++ src/app/zaya-portfolio-detail/zaya-portfolio-detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
-  selector: 'app-zaya-portfolio-detail',
+  selector: 'zaya-portfolio-detail',
   templateUrl: './zaya-portfolio-detail.component.html',
   styleUrls: ['./zaya-portfolio-detail.component.css']
 })
@@ -9,7 +8,6 @@ export class ZayaPortfolioDetailComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {;
   }
-
 }
